683da1daa9a20ecdc44999705ab56527db3ea8b4
Kazz <i94773007@gmail.com>
2025-01-19 13:13:11 +0100

    Added NPC data and enhanced CharacterCard component with descriptions and improved styling

---
+++ src/data/npcs.ts
@@ -0,0 +1,38 @@
+
+export default [
+	{
+		name: "Arik Lorensk",
+		description: "El camarero de \"La Sangre en(de) la Viña\", no parece prestar mucha atención, pero vende un pastel de frambuesa muy rico.",
+		image: "/characters/npcs/arik_lorensk.jpg"
+	},
+	{
+		name: "Dushan",
+		description: "Es el hijo del padre Donavich, ambos clérigos de la iglesia del Señor de la Mañana.",
+		image: "/characters/npcs/dushan.jpg"
+	},
+	{
+		name: "Ismark \"el menor\" Kolyanovich",
+		description: "Ismark es ahora el nuevo burgomaestre de Barovia.",
+		image: "/characters/npcs/ismark_kolyanovich.jpg"
+	},
+	{
+		name: "Ireena Kolyana",
+		description: "Ireena es la hija del anterior burgomaestre de Barovia, Kolyan Indirovich.",
+		image: "/characters/npcs/ireena_kolyana.jpg"
+	},
+	{
+		name: "Padre Donavich",
+		description: "Padre del clérigo Dushan, ya difunto, no llegasteis a conocerle.",
+		image: "/characters/npcs/father_donavich.jpg"
+	},
+	{
+		name: "Strahd von Zarovich",
+		description: "Un vampiro con mucha influencia en Barovia. Se rumorea que un druida convocó el poder de las estrellas para chamuscarle la mano.",
+		image: "/characters/npcs/strahd_von_zarovich.jpg"
+	},
+	{
+		name: "Alenka, Mirabel y Sorvia",
+		description: "Las tres hermanas Vistani que gerentan la taberna \"La Sangre en(de) la Viña\" en la aldea de Barovia.",
+		image: "/characters/npcs/alenka_mirabel_and_sorvia.jpg"
+	}
+]
