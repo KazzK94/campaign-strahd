192d38841883352c17c1f6c4afc6d934d7c5a6c3
Kazz <i94773007@gmail.com>
2025-01-26 12:52:41 +0100

    updated lore to (almost) complete chapter 1

--- src/data/lore.ts
+++ src/data/lore.ts
@@ -33,7 +33,7 @@ export default {
 			},
 			{
 				title: 'La entrega de la caja',
-				text: 'En ese mismo momento, una figura emergió de la niebla: un hombre alto, con ojos llameantes que reflejaban el vacío de su alma, y una sonrisa torcida que parecía conocer todos sus secretos. "Bienvenidos a Barovia", dijo su voz grave y resonante. "El destino los ha reunido aquí, como lo ha hecho con muchos antes que ustedes. Y como con ellos, el reino los probará... hasta que ya no queden más decisiones que tomar". Con un gesto lento, el hombre sacó un enorme rubí y siguió hablando. “Aquí esta vuestro pago” dijo, sacando un enorme rubí y dejándolo frente a ellos. “La caja ahora es vuestra, forma parte del pago, su contenido os será útil en vuestro destino próximo...” y se desvaneció en la niebla, dejando al grupo de viajeros ante la puerta de un futuro oscuro y retorcido.'
+				text: 'En ese mismo momento, una figura emergió de la niebla: un hombre alto, con ojos llameantes que reflejaban el vacío de su alma, y una sonrisa torcida que parecía conocer todos sus secretos. "Bienvenidos a Barovia", dijo su voz grave y resonante. "El destino los ha reunido aquí, como lo ha hecho con muchos antes que ustedes. Y como con ellos, el reino los probará... hasta que ya no queden más decisiones que tomar". Con un gesto lento, el hombre sacó un enorme rubí y siguió hablando. "Aquí está vuestro pago" dijo, sacando un enorme rubí y dejándolo frente a ellos. "La caja ahora es vuestra, forma parte del pago, su contenido os será útil en vuestro destino próximo..." y se desvaneció en la niebla, dejando al grupo de viajeros ante la puerta de un futuro oscuro y retorcido.'
 			},
 			{
 				title: 'El destino de los viajeros',
@@ -46,19 +46,59 @@ export default {
 		content: [
 			{
 				title: 'La caja y el rubí',
-				text: 'Tras el extraño encuentro con el cliente, el cual desapareció tal y como había llegado, el grupo recibió la caja y el rubí. Pterox optó por abrir la caja, encontrando allí una trampa que llegó a herirle a él y a algunos de sus compañeros mediante una descarga de rayos, pero a su vez allí encontraron armas de plata para todos los integrantes del grupo. En cuanto al rubí, Ava se ofreció como portadora del mismo, mientras sus ojos brillaban como si fuese una niña pequeña que había encontrado su juguete favorito.'
+				text: 'Tras el extraño encuentro con el extraño ser, que desapareció tal y como había llegado, el grupo recibió la caja y el rubí. Pterox optó por abrir la caja, encontrando allí una trampa que llegó a herirle a él y a algunos de sus compañeros mediante una descarga de rayos, pero a su vez allí encontraron armas de plata para todos los integrantes del grupo. En cuanto al rubí, Ava se ofreció como portadora del mismo, mientras sus ojos brillaban como si fuese una niña pequeña que había encontrado su juguete favorito.'
 			},
 			{
 				title: 'Las puertas de Barovia',
-				text: 'Una misteriosa niebla comenzó a rodear el camino, casi como indicándoles por dónde debían ir para continuar. Este camino terminaba en un gran portón que aparentemente estaba cerrado. Al mismo tiempo, unos aullidos de lobos se escuchaban en la otra dirección. Tratando de evitar el encuentro, el grupo abandonó el carro que portaba la caja y avanzó hasta la puerta y, después de un fútil intento de abrirla de modos absurdos y con impactos arcanos, la paladina tomó el liderazgo y simplemente… empujó. Así la puerta se abrió en un instante y los cinco pasaron al otro lado, viendo como esta se cerraba tras de sí de forma brusca, y al momento se reparaba de los daños que acababa de sufrir. Ava mencionó que había notado una distorsión como si el propio tiempo hubiese vuelto hacia atrás para esa puerta.'
+				text: 'Una misteriosa niebla comienza a rodear el camino, casi como indicándoles por dónde continuar. Este camino termina en un gran portón que aparentemente está cerrado. Al mismo tiempo, unos aullidos de lobos se escuchan en la otra dirección. Tratando de evitar el encuentro, el grupo deja atrás el carro que porta la caja y avanza hasta la puerta. Después de un fútil intento de abrirla de modos absurdos y con impactos arcanos, la paladina toma el liderazgo y simplemente… empuja. Así la puerta se abre en un instante y los cinco pasan al otro lado, viendo como esta se cierra tras de sí de forma brusca y, al momento, repara los daños que acaba de sufrir. Ava menciona que ha notado una distorsión como si el propio tiempo hubiese vuelto hacia atrás para esa puerta.'
 			},
 			{
 				title: 'Bailando con(tra) lobos',
 				text: 'La escena que descubren dentro no es menos peligrosa. Se han librado las bestias del camino, pero dentro un hombre lidia una batalla con otro grupo de 5-6 lobos que le rodea. El combate no tiene el mejor comienzo para los aventureros, los primeros golpes son fallidos o débiles por su parte, el kenku acaba derribado y el monje queda en el punto de mira de uno de los lobos. Pero pronto remontan la lucha a su favor y logran ayudar al hombre que combatía a los lobos en primera instancia, acabando además entre el pícaro y la bruja con los lobos que pretendían huir del combate.'
 			},
 			{
-				title: 'CONTINUARÁ...',
-				text: '(Sección en proceso)'
+				title: 'Dushan, el clérigo de la mañana',
+				text: 'El hombre al que han salvado se presenta como Dushan Donovich, clérigo del dios de la mañana. Porta un un colgante de un león. Nos explica que lleva tiempo esperando a alguien para prestarles ayuda y ya estaba a punto de rendirse. Agotado por el esfuerzo, entrega una carta.'
+			},
+			{
+				title: 'La carta de Kolyan Indirovich',
+				text: 'La carta es de Kolyan Indirovich, el burgomaestre de la aldea de Barovia. En ella se explica que su hija, Ireena Kolyana, ha sido mordida por un vampiro y necesita ayuda. Dushan les ofrece llevarles a la aldea, pero les advierte que no es un lugar seguro y que no podrá acompañarles más allá de la entrada. El grupo acepta y se dirigen a la aldea de Barovia.'
+			},
+			{
+				title: 'La Sangre de la Viña (primera visita)',
+				text: 'Al llegar a la aldea, el grupo se dirige a la taberna, donde conocen a Ismark, el hijo del burgomaestre Kolyan Indirovich y hermano de Ireena (la mujer a la que se hacía referencia en la carta, que había sido mordida). Ismark les informa de que su padre, el burgomaestre, ha muerto y quiere llevarlo a enterrar a la iglesia, pero Dushan se estremece con la idea y sugiere posponer el entierro, alegando que debe hablarlo con su padre primero. Mientras deciden qué hacer, los aventureros aprovechan para tomar algo en la taberna (¡invita la casa!). Ava aprovecha para pedir una tarta de frambuesa que le resulta mágicamente deliciosa (y aparentemente inofensiva), mientras que las tres hermanas Vistani aprovechan para coquetear con Diago, a lo que el tabernero responde poniendo mala cara y murmurando alguna palabra despectiva hacia ellas.'
+			},
+			{
+				title: 'Decisiones, decisiones',
+				text: 'Al no ponerse de acuerdo en qué hacer primero, el grupo se divide en dos: Pterok y Rionak acompañan a Ismark a la Mansión donde yace su padre fallecido, mientras que Ava, Diago y Ayla se quedan en la taberna para descansar.'
+			},
+			{
+				title: 'La Mansión del Burgomaestre',
+				text: 'Al llegar a la mansión Pterox y Rionak encuentran el ataúd del difunto burgomaestre. En la sala se encuentra también Ireena, despierta y aparentemente todavía bastante humana. Ishmark presenta a Ireena a sus dos nuevos compañeros, cuando alguien llama a la puerta. Rionak se acerca y pregunta antes de abrir. "Soy el amo de la tierra" se escucha a alguien responder desde fuera. Poco después, la puerta se abre de golpe. Una figura de un rostro pálido, de pelo negro como la noche y envuelto con una capa negra y roja, flota sobre el suelo frente a la puerta. "¿Me invitáis a entrar?" pregunta esta figura, en calma. Tras la reticencia de quienes están dentro, éste opta por la alternativa: "que así sea, que sean mis súbditos los que entren por mí". Una horda de no-muertos surge de la tierra a su señal. "Yo soy Strahd. Yo soy la Tierra" grita, mientras comanda a sus súbditos para iniciar la incursión.'
+			},
+			{
+				title: 'El combate en la Mansión',
+				text: 'Los ocupantes de la mansión cierran puertas y ventanas y esperan armados el ataque de sus enemigos. Una docena de no-muertos y un engendro vampírico comienzan una ofensiva. Rionak y Pterox defienden su posición como pueden, pero la desventaja numérica es muy alta y optan por salir por una de las ventanas que sus enemigos habían logrado romper, tras vencer al no-muerto que la bloqueaba.'
+			},
+			{
+				title: 'Mientras tanto, en la taberna...',
+				text: 'Los otros tres aventureros están descansando en las habitaciones de la taberna, pero una llamada a la puerta los alerta. Una de las camareras les avisa, diciendo que deberían ir a ayudar a sus amigos. Tras tomar armas se disponen a correr hacia la mansión. En la distancia ya se aprecia la mansión siendo atacada, y una figura les provoca con aires de superioridad. Tras dar un rodeo para evitar un encuentro frontal, llegan a la mansión. Un cuervo iluminado revoloteaba arriba de la misma, pero no tarda en ser fulminado por un rayo controlado por Strahd, el cual se jacta diciendo que deberían jugar limpio o no sería justo.'
+			},
+			{
+				title: 'El combate en la Mansión (parte 2)',
+				text: 'Se libra una dura batalla entre estos aventureros recién llegados a Barovia y una horda de no-muertos, y entre espinas, puñaladas, hostias con la mano abierta, tajos radiantes y fuego, la balanza comienza a equilibrarse. La mansión termina ardiendo mientras el engendro huye al tejado, a su vez siendo perseguido por Diago que acaba con él en un frenético ping-pong de dardos apoyado por la jabalina lanzada por la paladina desde abajo. Mientras tanto, gracias a sus súbditos que habían entrado en la mansión, Strahd logra hacerse con el cuerpo del burgomaestre y se ofrece a entregarlo a cambio de Ireena. No sólo nadie acepta, sino que Rionak logra impactar críticamente la mano de Strahd, que además recibe un rayo caído del cielo, y otro viniendo de la dirección opuesta (este último resultaría ser obra de Dushan, que acababa de llegar). Esto logra que Strahd suelte el cuerpo del burgomaestre y que, por alguna razón cínica y oscura, el vampiro se sienta complacido por el valor del druida. Terminada esta escena, la mansión termina de prenderse y el vampiro se esfuma, con lo que tanto los vivos como el difunto burgomaestre quedan libres de las garras del villano… por ahora.'
+			},
+			{
+				title: 'Taberna, segunda visita',
+				text: 'A la vuelta a la taberna, Dushan les dice que su padre ha muerto, y que su hermano (que había sido mordido) se ha fugado del sótano de la iglesia. Tras una lúgubre conversación, y otra cata de deliciosa tarta de frambuesa por parte de Ava, el grupo marcha a descansar.'
+			},
+			{
+				title: 'El entierro del burgomaestre',
+				text: 'Al día siguiente, el grupo se dirige a la iglesia para enterrar al burgomaestre. Mientras se realiza la ceremonia, el grupo aprende que por toda Barovia hay gente que está en contra de Strahd, pero también muchos otros son sus fieles seguidores. También descubren que las historias de las distintas personas del pueblo no coinciden en el tiempo.'
+			},
+			{
+				title: 'Venga, ¡a tomar por culo ya!',
+				text: 'Nos entregan un mapa y provisiones para el camino, y no sé muy bien por qué pero bueno, nos vamos a hacer algo a algún sitio xDD'
 			}
 		]
 	}
